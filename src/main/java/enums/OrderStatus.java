5e5d45e79fe9a8a9ca101bf5532272f85e8ddf37
Adrian <adrian.hamzah15@gmail.com>
2024-03-06 19:32:34 +0700

    [REFACTOR] Add OrderStatus enum

--- src/main/java/enums/OrderStatus.java
+++ src/main/java/enums/OrderStatus.java
@@ -7,7 +7,7 @@ public enum OrderStatus {
     WAITING_PAYMENT("WAITING_PAYMENT"),
     FAILED("FAILED"),
     SUCCESS("SUCCESS"),
-    CANCELLED("CANCELLED");;
+    CANCELED("CANCELED");;
 
     private final String value;
 
